aded072b2f0e394e8ef631c6a47d57fdd8de4bd5
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-07-16 09:43:41 -0700

    Add project files.

---
+++ CourtneyHutchinTournamentBrackets/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace CourtneyHutchinTournamentBrackets
+{
+    partial class TournamentBracketForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Game1Slot1Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot2Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot4Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot3Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot6Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot5Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot8Txt = new System.Windows.Forms.TextBox();
+            this.Game1Slot7Txt = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Game2Slot1Txt = new System.Windows.Forms.TextBox();
+            this.Game2Slot2Txt = new System.Windows.Forms.TextBox();
+            this.Game2Slot3Txt = new System.Windows.Forms.TextBox();
+            this.Game2Slot4Txt = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Game3Slot1Txt = new System.Windows.Forms.TextBox();
+            this.Game3Slot2Txt = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.FirstPlaceTxt = new System.Windows.Forms.TextBox();
+            this.SecondPlaceTxt = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CreateTournamentBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Game1Slot1Txt
+            // 
+            this.Game1Slot1Txt.Location = new System.Drawing.Point(19, 24);
+            this.Game1Slot1Txt.Name = "Game1Slot1Txt";
+            this.Game1Slot1Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot1Txt.TabIndex = 0;
+            // 
+            // Game1Slot2Txt
+            // 
+            this.Game1Slot2Txt.Location = new System.Drawing.Point(19, 67);
+            this.Game1Slot2Txt.Name = "Game1Slot2Txt";
+            this.Game1Slot2Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot2Txt.TabIndex = 1;
+            // 
+            // Game1Slot4Txt
+            // 
+            this.Game1Slot4Txt.Location = new System.Drawing.Point(19, 178);
+            this.Game1Slot4Txt.Name = "Game1Slot4Txt";
+            this.Game1Slot4Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot4Txt.TabIndex = 3;
+            // 
+            // Game1Slot3Txt
+            // 
+            this.Game1Slot3Txt.Location = new System.Drawing.Point(19, 135);
+            this.Game1Slot3Txt.Name = "Game1Slot3Txt";
+            this.Game1Slot3Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot3Txt.TabIndex = 2;
+            // 
+            // Game1Slot6Txt
+            // 
+            this.Game1Slot6Txt.Location = new System.Drawing.Point(19, 296);
+            this.Game1Slot6Txt.Name = "Game1Slot6Txt";
+            this.Game1Slot6Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot6Txt.TabIndex = 5;
+            // 
+            // Game1Slot5Txt
+            // 
+            this.Game1Slot5Txt.Location = new System.Drawing.Point(19, 253);
+            this.Game1Slot5Txt.Name = "Game1Slot5Txt";
+            this.Game1Slot5Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot5Txt.TabIndex = 4;
+            // 
+            // Game1Slot8Txt
+            // 
+            this.Game1Slot8Txt.Location = new System.Drawing.Point(19, 409);
+            this.Game1Slot8Txt.Name = "Game1Slot8Txt";
+            this.Game1Slot8Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot8Txt.TabIndex = 7;
+            // 
+            // Game1Slot7Txt
+            // 
+            this.Game1Slot7Txt.Location = new System.Drawing.Point(19, 366);
+            this.Game1Slot7Txt.Name = "Game1Slot7Txt";
+            this.Game1Slot7Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game1Slot7Txt.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Game1Slot8Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot7Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot6Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot5Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot4Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot3Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot2Txt);
+            this.groupBox1.Controls.Add(this.Game1Slot1Txt);
+            this.groupBox1.Location = new System.Drawing.Point(27, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 454);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game One";
+            // 
+            // Game2Slot1Txt
+            // 
+            this.Game2Slot1Txt.Location = new System.Drawing.Point(9, 19);
+            this.Game2Slot1Txt.Name = "Game2Slot1Txt";
+            this.Game2Slot1Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game2Slot1Txt.TabIndex = 8;
+            // 
+            // Game2Slot2Txt
+            // 
+            this.Game2Slot2Txt.Location = new System.Drawing.Point(9, 129);
+            this.Game2Slot2Txt.Name = "Game2Slot2Txt";
+            this.Game2Slot2Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game2Slot2Txt.TabIndex = 9;
+            // 
+            // Game2Slot3Txt
+            // 
+            this.Game2Slot3Txt.Location = new System.Drawing.Point(9, 246);
+            this.Game2Slot3Txt.Name = "Game2Slot3Txt";
+            this.Game2Slot3Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game2Slot3Txt.TabIndex = 10;
+            // 
+            // Game2Slot4Txt
+            // 
+            this.Game2Slot4Txt.Location = new System.Drawing.Point(9, 363);
+            this.Game2Slot4Txt.Name = "Game2Slot4Txt";
+            this.Game2Slot4Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game2Slot4Txt.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Game2Slot4Txt);
+            this.groupBox2.Controls.Add(this.Game2Slot3Txt);
+            this.groupBox2.Controls.Add(this.Game2Slot2Txt);
+            this.groupBox2.Controls.Add(this.Game2Slot1Txt);
+            this.groupBox2.Location = new System.Drawing.Point(178, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(121, 427);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Game Two";
+            // 
+            // Game3Slot1Txt
+            // 
+            this.Game3Slot1Txt.Location = new System.Drawing.Point(7, 45);
+            this.Game3Slot1Txt.Name = "Game3Slot1Txt";
+            this.Game3Slot1Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game3Slot1Txt.TabIndex = 12;
+            // 
+            // Game3Slot2Txt
+            // 
+            this.Game3Slot2Txt.Location = new System.Drawing.Point(7, 278);
+            this.Game3Slot2Txt.Name = "Game3Slot2Txt";
+            this.Game3Slot2Txt.Size = new System.Drawing.Size(100, 20);
+            this.Game3Slot2Txt.TabIndex = 13;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Game3Slot2Txt);
+            this.groupBox3.Controls.Add(this.Game3Slot1Txt);
+            this.groupBox3.Location = new System.Drawing.Point(305, 108);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(117, 398);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "GameThree";
+            // 
+            // FirstPlaceTxt
+            // 
+            this.FirstPlaceTxt.Location = new System.Drawing.Point(16, 104);
+            this.FirstPlaceTxt.Name = "FirstPlaceTxt";
+            this.FirstPlaceTxt.Size = new System.Drawing.Size(100, 20);
+            this.FirstPlaceTxt.TabIndex = 14;
+            // 
+            // SecondPlaceTxt
+            // 
+            this.SecondPlaceTxt.Location = new System.Drawing.Point(16, 158);
+            this.SecondPlaceTxt.Name = "SecondPlaceTxt";
+            this.SecondPlaceTxt.Size = new System.Drawing.Size(100, 20);
+            this.SecondPlaceTxt.TabIndex = 15;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.SecondPlaceTxt);
+            this.groupBox4.Controls.Add(this.FirstPlaceTxt);
+            this.groupBox4.Location = new System.Drawing.Point(427, 147);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(137, 357);
+            this.groupBox4.TabIndex = 16;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Winners";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Second Place";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "First Place";
+            // 
+            // CreateTournamentBtn
+            // 
+            this.CreateTournamentBtn.Location = new System.Drawing.Point(587, 251);
+            this.CreateTournamentBtn.Name = "CreateTournamentBtn";
+            this.CreateTournamentBtn.Size = new System.Drawing.Size(130, 74);
+            this.CreateTournamentBtn.TabIndex = 17;
+            this.CreateTournamentBtn.Text = "Create Tournament";
+            this.CreateTournamentBtn.UseVisualStyleBackColor = true;
+            this.CreateTournamentBtn.Click += new System.EventHandler(this.CreateTournamentBtn_Click);
+            // 
+            // TournamentBracketForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 547);
+            this.Controls.Add(this.CreateTournamentBtn);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TournamentBracketForm";
+            this.Text = "Tournament Bracket Form";
+            this.Load += new System.EventHandler(this.TournamentBracketForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Game1Slot1Txt;
+        private System.Windows.Forms.TextBox Game1Slot2Txt;
+        private System.Windows.Forms.TextBox Game1Slot4Txt;
+        private System.Windows.Forms.TextBox Game1Slot3Txt;
+        private System.Windows.Forms.TextBox Game1Slot6Txt;
+        private System.Windows.Forms.TextBox Game1Slot5Txt;
+        private System.Windows.Forms.TextBox Game1Slot8Txt;
+        private System.Windows.Forms.TextBox Game1Slot7Txt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Game2Slot1Txt;
+        private System.Windows.Forms.TextBox Game2Slot2Txt;
+        private System.Windows.Forms.TextBox Game2Slot3Txt;
+        private System.Windows.Forms.TextBox Game2Slot4Txt;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Game3Slot1Txt;
+        private System.Windows.Forms.TextBox Game3Slot2Txt;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox FirstPlaceTxt;
+        private System.Windows.Forms.TextBox SecondPlaceTxt;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CreateTournamentBtn;
+    }
+}
+
---
+++ CourtneyHutchinTournamentBrackets/Form1.cs
@@ -0,0 +1,258 @@
+﻿using CourtneyHutchinTournamentBrackets.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Courtney Hutchin
+// CPW 205
+// Brackets Program Phase One
+
+// NOTE: The groupboxes are just used on the form itself for a break down of each game visually
+
+namespace CourtneyHutchinTournamentBrackets
+{
+    public partial class TournamentBracketForm : Form
+    {
+        // Arrays to hold pieces of the text file
+        private List<string> tournamentElementInfo;
+        private string[,] tournament;
+
+        // Player info
+        private const int PlayerName = 0;
+        private const int gameOneScore = 1;
+        private const int gameTwoScore = 2;
+        private const int gameThreeScore = 3;
+        
+        // Players
+        private int PlayerOne { get; set; }
+        private int PlayerTwo { get; set; }
+        private int PlayerThree { get; set; }
+        private int PlayerFour { get; set; }
+        private int PlayerFive { get; set; }
+        private int PlayerSix { get; set; }
+        private int PlayerSeven { get; set; }
+        private int PlayerEight { get; set; }
+
+        public TournamentBracketForm()
+        {
+            InitializeComponent();
+
+            // Grab the tournament text file
+            string filePath = Resources.tournament;
+
+            // Create a list to hold elements from txt file in
+            tournamentElementInfo = new List<String>();
+
+            // https://docs.microsoft.com/en-us/dotnet/csharp/how-to/parse-strings-using-split
+            // Split each line into the new array
+            string[] file = filePath.Split(new[]{Environment.NewLine},StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            // Loop through the line and split each element
+            foreach (string line in file)
+            {
+                // split by using the whitespace
+                string[] linesAsArray = line.Split(' ');
+
+                // Loop through each element in the array
+                foreach (string word in linesAsArray)
+                {
+                    // Add into array
+                    tournamentElementInfo.Add(word);
+                }
+            }
+        }
+
+        private void TournamentBracketForm_Load(object sender, EventArgs e)
+        {
+            int numPlayers = 8;    // There are eight lines one player per line
+            int numPlayerInfo = 4; // There are four pieces of info per line in the tournament txt file
+
+            // Create a 2d array to hold players and their info(scores)
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays
+            string[,] tournamentInfoArr = new string[numPlayers, numPlayerInfo];
+
+            // Hold the index of the file so the info gets added in the correct order 
+            int fileListIndex = 0;
+
+            // Loop through the array and add the values to another array
+            for (int i = 0; i < numPlayers; i++) {
+                for (int j = 0; j < numPlayerInfo; j++)
+                {
+                    tournamentInfoArr[i, j] = tournamentElementInfo[fileListIndex];
+                    fileListIndex++;
+                }
+            }
+            tournament = tournamentInfoArr;
+        }
+
+        private void CreateTournamentBtn_Click(object sender, EventArgs e)
+        {
+            // Create a list to store the players in
+            List<int> newPlayerNum = new List<int>();
+
+            // Assign the players numbers so that it can be randoms
+            PlayerOne = GetRandomPlayerNum(newPlayerNum);
+            PlayerTwo = GetRandomPlayerNum(newPlayerNum);
+            PlayerThree = GetRandomPlayerNum(newPlayerNum);
+            PlayerFour = GetRandomPlayerNum(newPlayerNum);
+            PlayerFive = GetRandomPlayerNum(newPlayerNum);
+            PlayerSix = GetRandomPlayerNum(newPlayerNum);
+            PlayerSeven = GetRandomPlayerNum(newPlayerNum);
+            PlayerEight = GetRandomPlayerNum(newPlayerNum);
+
+            // Add text into form
+            Game1Slot1Txt.Text = $"{tournament[PlayerOne, PlayerName]} {tournament[PlayerOne, gameOneScore]}";
+            Game1Slot2Txt.Text = $"{tournament[PlayerTwo, PlayerName]} {tournament[PlayerTwo, gameOneScore]}";
+            Game1Slot3Txt.Text = $"{tournament[PlayerThree, PlayerName]} {tournament[PlayerThree, gameOneScore]}";
+            Game1Slot4Txt.Text = $"{tournament[PlayerFour, PlayerName]} {tournament[PlayerFour, gameOneScore]}";
+            Game1Slot5Txt.Text = $"{tournament[PlayerFive, PlayerName]} {tournament[PlayerFive, gameOneScore]}";
+            Game1Slot6Txt.Text = $"{tournament[PlayerSix, PlayerName]} {tournament[PlayerSix, gameOneScore]}";
+            Game1Slot7Txt.Text = $"{tournament[PlayerSeven, PlayerName]} {tournament[PlayerSeven, gameOneScore]}";
+            Game1Slot8Txt.Text = $"{tournament[PlayerEight, PlayerName]} {tournament[PlayerEight, gameOneScore]}";
+
+            // Broken down per sub-bracket p1 vs p2. p3 vs p4 etc
+            int gameOneWinnerOne;
+            int gameOneWinnerTwo;
+            int gameOneWinnerThree;
+            int gameOneWinnerFour;
+
+            // Winners from game 2
+            int gameTwoWinnerOne;
+            int gameTwoWinnerTwo;
+
+            // Finialists
+            int firstPlaceWinner;
+            int secondPlaceWinner;
+
+            // Check for winner of player 1 vs player 2
+            if (Convert.ToInt32(tournament[PlayerOne, gameOneScore]) > Convert.ToInt32(tournament[PlayerTwo, gameOneScore]))
+            {
+                Game2Slot1Txt.Text = $"{tournament[PlayerOne, PlayerName]} {tournament[PlayerOne, gameTwoScore]}";
+                gameOneWinnerOne = PlayerOne;
+            }
+            else
+            {
+                Game2Slot1Txt.Text = $"{tournament[PlayerTwo, PlayerName]} {tournament[PlayerTwo, gameTwoScore]}";
+                gameOneWinnerOne = PlayerTwo;
+            }
+
+            // 3 vs 4
+            if (Convert.ToInt32(tournament[PlayerThree, gameOneScore]) > Convert.ToInt32(tournament[PlayerFour, gameOneScore]))
+            {
+                Game2Slot2Txt.Text = $"{tournament[PlayerThree, PlayerName]} {tournament[PlayerThree, gameTwoScore]}";
+                gameOneWinnerTwo = PlayerThree;
+            }
+            else
+            {
+                Game2Slot2Txt.Text = $"{tournament[PlayerFour, PlayerName]} {tournament[PlayerFour, gameTwoScore]}";
+                gameOneWinnerTwo = PlayerFour;
+            }
+
+            // 5 vs 6
+            if (Convert.ToInt32(tournament[PlayerFive, gameOneScore]) > Convert.ToInt32(tournament[PlayerSix, gameOneScore]))
+            {
+                Game2Slot3Txt.Text = $"{tournament[PlayerFive, PlayerName]} {tournament[PlayerFive, gameTwoScore]}";
+                gameOneWinnerThree = PlayerFive;
+            }
+            else
+            {
+                Game2Slot3Txt.Text = $"{tournament[PlayerSix, PlayerName]} {tournament[PlayerSix, gameTwoScore]}";
+                gameOneWinnerThree = PlayerSix;
+            }
+
+            // 7 vs 8
+            if (Convert.ToInt32(tournament[PlayerSeven, gameOneScore]) > Convert.ToInt32(tournament[PlayerEight, gameOneScore]))
+            {
+                Game2Slot4Txt.Text = $"{tournament[PlayerSeven, PlayerName]} {tournament[PlayerSeven, gameTwoScore]}";
+                gameOneWinnerFour = PlayerSeven;
+            }
+            else
+            {
+                Game2Slot4Txt.Text = $"{tournament[PlayerEight, PlayerName]} {tournament[PlayerEight, gameTwoScore]}";
+                gameOneWinnerFour = PlayerEight;
+            }
+
+            //                                          GAME TWO
+
+            // Game 2 bracket 1 winner vs Game 2 bracket 2 winner
+            if (Convert.ToInt32(tournament[gameOneWinnerOne, gameTwoScore]) > Convert.ToInt32(tournament[gameOneWinnerTwo, gameTwoScore]))
+            {
+                Game3Slot1Txt.Text = $"{tournament[gameOneWinnerOne, PlayerName]} {tournament[gameOneWinnerOne, gameThreeScore]}";
+                gameTwoWinnerOne = gameOneWinnerOne;
+            }
+            else
+            {
+                Game3Slot1Txt.Text = $"{tournament[gameOneWinnerTwo, PlayerName]} {tournament[gameOneWinnerTwo, gameThreeScore]}";
+                gameTwoWinnerOne = gameOneWinnerTwo;
+            }
+
+            // Game 2 bracket 3 winner vs Game 2 bracket 4 winner
+            if (Convert.ToInt32(tournament[gameOneWinnerThree, gameTwoScore]) > Convert.ToInt32(tournament[gameOneWinnerFour, gameTwoScore]))
+            {
+                Game3Slot2Txt.Text = $"{tournament[gameOneWinnerThree, PlayerName]} {tournament[gameOneWinnerThree, gameThreeScore]}";
+                gameTwoWinnerTwo = gameOneWinnerThree;
+            }
+            else
+            {
+                Game3Slot2Txt.Text = $"{tournament[gameOneWinnerFour, PlayerName]} {tournament[gameOneWinnerFour, gameThreeScore]}";
+                gameTwoWinnerTwo = gameOneWinnerFour;
+            }
+
+            //                                          GAME THREE
+
+            if (Convert.ToInt32(tournament[gameTwoWinnerOne, gameThreeScore]) > Convert.ToInt32(tournament[gameTwoWinnerTwo, gameThreeScore]))
+            {
+                firstPlaceWinner = gameTwoWinnerOne;
+                secondPlaceWinner = gameTwoWinnerTwo;
+            }
+            else
+            {
+                firstPlaceWinner = gameTwoWinnerTwo;
+                secondPlaceWinner = gameTwoWinnerOne;
+            }
+
+            // Display winners
+            FirstPlaceTxt.Text = $"{tournament[firstPlaceWinner, PlayerName]} $25";
+            SecondPlaceTxt.Text = $"{tournament[secondPlaceWinner, PlayerName]} $10";
+        }
+
+        /// <summary>
+        /// Gives a player a random number
+        /// </summary>
+        /// <param name="assignPlayerNums"></param>
+        /// <returns></returns>
+        private int GetRandomPlayerNum(List<int> newPlayerNum)
+        {
+            Random randNum = new Random();
+
+            bool goAgain = true;
+            int playerNum = -1;
+
+            while (goAgain)
+            {
+                // get a random num 
+                int num = randNum.Next(0, 8);
+
+                // if nobody has that number...assign it
+                if (!newPlayerNum.Contains(num))
+                {
+                    newPlayerNum.Add(num);
+                    goAgain = false;
+                    playerNum = num;
+                }
+                else // if someone DOES have that number
+                {
+                    goAgain = true; // go through the loop again
+                }
+            }
+            return playerNum;
+        }
+    }
+}
